fef2c8b09e65beaa239243223aaadbb39451479d
Matt Knox <matt.knox@sphero.com>
2016-06-14 10:02:21 -0600

    Adding metriks library

--- lib/exceptionhandler.rb
+++ lib/exceptionhandler.rb
@@ -1,8 +1,45 @@
+require 'metriks'
+require 'logger'
+require 'metriks/reporter/logger'
+require 'unirest'
+
+
 class ExceptionHandler
+
+  def initialize()
+    #Get the cluster name from Google metadata service
+    cluster_name="unknown_cluster"
+    begin
+      response = Unirest.get('http://metadata.google.internal/computeMetadata/v1/instance/attributes/cluster-name',headers:{ "Metadata-Flavor" => "Google"})
+      if response.code == 200
+        cluster_name=response.body
+      end
+    rescue
+      p "Couldn't determine cluster name."
+    end
+
+    #Grap the rest of the details from tags
+    environment = ENV['environment'] ? ENV['environment'] : "unknown_env"
+    app = ENV['DEIS_APP'] ? ENV['DEIS_APP'] : "unknown_app"
+    release = ENV['WORKFLOW_RELEASE'] ? ENV['WORKFLOW_RELEASE'] : "unknown_release"
+
+    #Format is cluster.env.appname.release.exception
+    @metric_prefix="#{cluster_name}.#{environment}.#{app}.#{release}"
+
+    p "Starting log reporter..."
+    log_reporter = Metriks::Reporter::Logger.new(:logger => Logger.new(STDOUT))
+    log_reporter.start
+    p "Logging metrics with prefix: " + @metric_prefix
+  end
+
   def call(options)
     if !options[:exception].nil?
       trace_chain = trace_chain(options[:exception])
       puts "Exception Caught ("+options[:exception].class.name+"): " + trace_chain.to_s
+
+     meter = Metriks.meter("#{@metric_prefix}.#{options[:exception].class.name}")
+     meter.mark
+      
     end
     
     #The whole point of this handler is to replace rollbar, so raise this exception to prevent trying to send to rollbar
